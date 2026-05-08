fb28c97790b1edf268d3937c606f0f358dac729d
noyrosenfeldst <noy@DESKTOP-20RAPNM>
2026-05-08 20:11:29 +0300

    added question5.py file

---
+++ question5.py
@@ -0,0 +1,25 @@
+def GetWordsFromFile(file_path):
+    """Return all unique words from a file."""
+
+    unique_words = set()
+
+    try:
+        with open(file_path, "r") as file:
+            file_content = file.read()
+
+            words = file_content.split()
+
+            for word in words:
+                unique_words.add(word)
+
+    except FileNotFoundError:
+        print("File was not found.")
+
+    except PermissionError:
+        print("No permission to access the file.")
+
+    return list(unique_words)
+
+
+# check
+print(GetWordsFromFile("question1.py"))
